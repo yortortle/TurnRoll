ab76e8763b45892fabfb59235058f85b4ce9d07f
Troy <tnoblehancock@gmail.com>
2020-11-28 11:26:50 -0500

    fixed gamebreaking data reading bug

--- Source/TurnRoll/NPCMerchant.cpp
+++ Source/TurnRoll/NPCMerchant.cpp
@@ -43,7 +43,8 @@ void ANPCMerchant::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 
 void ANPCMerchant::OnOverLapBegins(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	GameInstance->InteractNPC = true;
+	//GameInstance->InteractNPC = true;
+
 	//UE_LOG(LogTemp, Warning, TEXT("Overlap Begins2"));
 	UE_LOG(LogTemp, Warning, TEXT("Overlap Begins3"));
 }
