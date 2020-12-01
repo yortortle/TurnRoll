2b715720d0df77b73d436161a6c34436b56fae8f
Troy <tnoblehancock@gmail.com>
2020-12-01 18:01:56 -0500

    Got yort character destroying the proper other targets on action

--- Source/TurnRoll/JeffCharacter.cpp
+++ Source/TurnRoll/JeffCharacter.cpp
@@ -23,4 +23,5 @@ void AJeffCharacter::Action()
 void AJeffCharacter::OnOverLapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Overlapjeff"));
+	//OtherActor.
 }
--- Source/TurnRoll/YortCharacter.cpp
+++ Source/TurnRoll/YortCharacter.cpp
@@ -8,6 +8,7 @@ void AYortCharacter::BeginPlay()
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("Yort begin play"));
 	HitBox->OnComponentBeginOverlap.AddDynamic(this, &AYortCharacter::OnOverLapBegin);
+	HitBox->SetCollisionProfileName("NoCollision");
 }
 
 void AYortCharacter::Action()
@@ -15,9 +16,22 @@ void AYortCharacter::Action()
 	//sets timer for attack animation which will return and stop my character from moving
 	GetWorldTimerManager().SetTimer(AttackTimer, this, &AMainCharacter::AttackTimerExecute, 1, false);
 	UE_LOG(LogTemp, Warning, TEXT("YortAction"));
+	HitBox->SetCollisionProfileName("OverlapAllDynamic");
+	HitBox->SetCollisionProfileName("NoCollision");
 }
 
 void AYortCharacter::OnOverLapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogTemp, Warning, TEXT("YortOverLap"));
+
+	//UE_LOG(LogTemp, Warning, TEXT("YortOverLap"));
+
+	//turning the collision box on
+
+	HitBox->SetCollisionProfileName("OverlapAllDynamic");
+	if (OtherActor->ActorHasTag("Tree"))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tree Collided"));
+		OtherActor->Destroy();
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Overlap"));
 }
